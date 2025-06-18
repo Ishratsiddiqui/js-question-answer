02ca8526a094960e6ba3097f22265462fe9debdb
ishrat <siddiqueishrat36@gmail.com>
2025-06-18 16:44:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// Question 1 answer
+var username
+// Question 2 answer
+var myName = "Ishrat Siddiqui";
+alert(myName);
+// Question 3 answer
+var message
+var message = "Hello World";
+alert(message);
+// Question 4 answer
+alert("Student's Bio");
+var StudentName = "Ishrat Siddiqui";
+alert(StudentName);
+var StudentAge = "42 yeras old";
+alert(StudentAge);
+var skill = "Certified web developer";
+alert(skill);
+
+// Question 5 answer
+var pizaa = "pizza \n pizz \n piz \n pi \n p";
+alert(pizaa);
+
+// Question 6 answer
+var text = "my email address is...";
+var email = "ishratsiddiqui184@gmail.com";
+var result = text.concat("",email);
+alert(result);
+
+// Question 7 answer
+var book = "A smarter way to learn Javascript";
+alert(book);
+
+// Question 8 answer
+document.write("yah! I can write HTML content through Javascript");
+
+// Question 9 answer
+document.write("-----------@#$&----------");
+
+// Question 10 answer
+alert("-----------@#$&-----------");
